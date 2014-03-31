c0384addc119d5ff610f64a2ca1d3212f14f87e8
Nick AMancini <nmancini11@gmail.com>
2014-03-30 23:06:29 -0700

    cleaned up the UI... trying to get away from Proparty's beautiful design

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 $(function () {
-  prettyPrint();
 
   $('.jumbotron h1').fitText(0.8, { minFontSize: 50, maxFontSize: 110 });
-
   // title
   for (var i = 1; i <= 8; i++) {
     pp('.char' + i, { duration: 10000, initialDelay: (400 * i), cycle: true, ease: 'out' })
     .set('color', [ '#C7F464', '#FF6B6B', '#C44D58' ])
     .start();
   }
+
+  // setup Audio
+  var skate = new Audio("audio/skate.mp3"), // buffers automatically when created
+      pop = new Audio("audio/pop.mp3");
+      skate.play();
 
   // easing types
   var types = ['in', 'out', 'in-out', 'linear', 'snap', 'ease-in-quad', 'ease-in-cubic', 'ease-in-quart', 'ease-in-quint', 'ease-in-sine', 'ease-in-expo', 'ease-in-circ', 'ease-in-back', 'ease-out-quart', 'ease-out-quint', 'ease-out-sine', 'ease-out-expo', 'ease-out-circ', 'ease-out-back', 'ease-out-quad', 'ease-out-cubic', 'ease-in-out-quart', 'ease-in-out-quint', 'ease-in-out-sine', 'ease-in-out-expo', 'ease-in-out-circ', 'ease-in-out-back'];
@@ -22,160 +25,110 @@ $(function () {
         ' .set(\'top\', \'80%\')',
         ' .start();'
       ].join('\n'));
-      prettyPrint();
     })
     .change();
 
-  var demos = {
-    basic: function (demo) {
-      return demo
-        .set('top', '80%')
-        .setWithVendor('border-radius', '100px');
-    },
-    multi: function (demo) {
-      return demo
-        .set('top', ['80%', '60%'])
-        .set('left', ['80%', '20%', '50%']);
-    },
-    cycle: function (demo) {
-      return demo
-        .setCycle(7)
-        .set('top', ['80%', '60%', '20%', '40%'])
-        .set('left', ['80%', '20%', '50%']);
-    },
-    timing: function (demo) {
-      return demo
-        .setSettings({ duration: 1000, delay: 250, initialDelay: 500 })
-        .set('padding', ['20%', '10%'])
-        .set('opacity', ['0.5', '1.0']);
-    },
-    ease: function (demo, ease) {
-      return demo
-        .setEase(ease)
-        .set('top', '80%');
-    },
-    transforms: function (demo) {
-      return demo
-        .set('top', ['40%','45%','45%', '50%'])
-        .transform('rotateX', ['-90deg','-180deg','-90deg','0deg'])
-        .transform('rotateZ', ['90deg','180deg','270deg','360deg']);
-    },
-    treflip: function (demo, ease) {
-      return demo
-        .setEase("ease-out-cubic")
-        .set('top', ['45%','50%'])
+  var tricks = {
+    treflip: function (trick, ease) {
+      return trick
+        .setSettings({ duration: 250, delay: 100, initialDelay: 50})
+        .set('top', ['23%','28%'])
         .transform('rotateZ', ['-360deg'])
         .transform('rotateX', ['360deg']);
     },
-    kickflip: function (demo) {
-      return demo
-        .set('top', ['45%','50%'])
-        .transform('rotateX', ['-360deg','-360deg'])
+    varialflip: function (trick, ease) {
+      return trick
+        .setSettings({ duration: 250, delay: 100, initialDelay: 50})
+        .set('top', ['23%','28%'])
+        .transform('rotateZ', ['-180deg'])
+        .transform('rotateX', ['360deg']);
     },
-    shuvit: function (demo) {
-      return demo
-        .set('top', ['45%','50%'])
-        .transform('rotateZ', ['-180deg','-180deg'])
+    kickflip: function (trick) {
+      return trick
+        .setSettings({ duration: 250})
+        .set('top', ['23%','28%'])
+        .transform('rotateX', ['-360deg','-360deg']);
     },
-    ollie: function (demo) {
-      return demo
-        .set('top', ['30%','25%','50%'])
-        .transform('rotateZ', ['-50deg','10deg'])
+    heelflip: function (trick) {
+      return trick
+        .setSettings({ duration: 250})
+        .set('top', ['23%','28%'])
+        .transform('rotateX', ['360deg','360deg']);
     },
-    animations: function (demo) {
-      return demo
-        .setDuration(1000)
-        .animate('twisterInDown');
+    shuvit: function (trick) {
+      return trick
+        .setSettings({ duration: 250})
+        .set('top', ['23%','28%'])
+        .transform('rotateZ', ['180deg','180deg']);
     },
-    functions: function (demo) {
-      return demo
-        .setCycle(5)
-        .set('top', function () { 
-          return Math.floor((Math.random() * 80) + 1) + '%'; 
-        })
-        .transform('rotate', [
-          function () { return (this.inc + 1) * 45 + 'deg'; },
-          function () { return (this.inc + 1) * 90 + 'deg'; }
-        ]);
-    },
-    addsub: function (demo) {
-      return demo
-        .setCycle(5)
-        .set('top', pp.math.subtract('40px'))
-        .set('left', pp.math.add('40px'));
-    },
-    methods: function (demo) {
-      return demo
-        .setCycle(true)
-        .set('top', ['15%', '85%', '85%', '15%'])
-        .set('left', ['15%', '15%', '85%', '85%'])
-        .set('background-color', function () { 
-          return '#' + (Math.random().toString(16) + '000000').slice(2, 8); 
-        });
-    },
-    callbacks: function (demo) {
-      return demo
-        .setCycle(6)
-        .transform('skew', ['20deg, 60deg', '-20deg, -60deg', '0deg, 0deg'])
-        .transform('rotateY', ['180deg', '180deg', '0deg'])
-        .whenStarted(function () { $('.demo-text').html('Started!'); })
-        .whenPaused(function () { $('.demo-text').html('Paused!'); })
-        .whenStopped(function () { $('.demo-text').html('Stopped!'); });
-    },
-     chaining: function (demo1, ease, demo2) {
-      return demo1
-        .set('top', ['20%', '80%'])
-        .set('left', '20%')
-        .chain(function () {
-          return demo2
-            .set('top', ['20%', '80%'])
-            .set('left', '80%')
-        });
+    manual: function (trick) {
+      return trick
+        .setSettings({ duration: 300})
+        .transform('rotateY',['-35deg', '-5deg','0deg'])
+        .transform('translateZ',['50px']);
     }
   };
 
-  var ppDemo1;
-  var ppDemo2;
+  var pptrick1;
+  var pptrick2;
 
   $('.play').on('click', function () {
-    var demo = $(this).data('play');
+    var trick = $(this).data('play');
 
-    $('.demo').attr('style', '');
-    $('.demo-text').text('');
+
+    $('.trick').attr('style', '');
+    $('.trick-text').text('');
 
     // data-controls is true like play / pause / stop / destroy
     // if it has them... show them. If not hide them
-    $('.demo-controls')[$(this).data('controls') ? 'show' : 'hide']();
+    // $('.trick-controls')[$(this).data('controls') ? 'show' : 'hide']();
 
-    $('.demo-controls .toggle')
-        .data('toggle', 'pause')
-        .text('Pause');
+    // $('.trick-controls .toggle')
+    //     .data('toggle', 'pause')
+    //     .text('Pause');
 
-    $('.demo-container').toggleClass('multiple', !!$(this).data('multiple'));
+    $('.trick-container').toggleClass('multiple', !!$(this).data('multiple'));
 
     // this is the Proparty function that executes the animation
-    pp('.demo-container')
+    pp('.trick-container')
       .whenStarted(function () {
-        $(this.element).show();
+        pop.play();
+        skate.pause().delay(10);
       })
-      .set('opacity', 1)
       .chain(function () {
-        // console.log('chain 1')
-        ppDemo1 = pp('.demo-one');
-        ppDemo2 = pp('.demo-two');
-        return demos[demo](ppDemo1, $('#easingSelect').val(), ppDemo2)
+        pptrick1 = pp('.trick-one');
+        pptrick2 = pp('.trick-two');
+        return tricks[trick](pptrick1, $('#easingSelect').val(), pptrick2)
           .chain(function () {
-            // console.log('chain 2')
-            return pp('.demo-container')
-              // .set('opacity', 0)
+            return pp('.trick-container')
               .whenDestroyed(function () {
-                // console.log('chain end');
-                // $(this.element).hide();
+                skate.play();
               });
           });
       })
       .start();
   });
+
+  // $('.manual').on('mouseover', function () {
+  //   var trick = $(this).data('play');
+
+
+  //   $('.trick').attr('style', '');
+  //   $('.trick-text').text('');
+
+  //   $('.trick-container').toggleClass('multiple', !!$(this).data('multiple'));
+
+  //   // this is the Proparty function that executes the animation
+  //   pp('.trick-container')
+  //     .chain(function () {
+  //       pptrick1 = pp('.trick-one');
+  //       return tricks[trick](pptrick1, $('#easingSelect').val(), pptrick2)
+  //         .chain(function () {
+  //           return pp('.trick-container');
+  //         });
+  //     })
+  //     .start();
+  // });
 
 
   // pp('.example', { cycle: true })
